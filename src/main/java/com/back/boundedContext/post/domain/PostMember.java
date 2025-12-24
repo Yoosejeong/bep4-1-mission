9097974ff0c0fc7f2d3f828802b8962cdc0788e3
YooSejeong <nived3@naver.com>
2025-12-24 16:01:25 +0900

    0017

--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -1,7 +1,6 @@
 package com.back.boundedContext.post.domain;
 
-import com.back.global.jpa.entity.BaseIdAndTimeManual;
-import jakarta.persistence.Column;
+import com.back.shared.member.domain.ReplicaMember;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.NoArgsConstructor;
@@ -9,10 +8,10 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @Table(name = "POST_MEMBER")
-public class PostMember extends BaseIdAndTimeManual {
-    @Column(unique = true)
-    private String username;
-    private String password;
-    private String nickname;
-    private int activityScore;
+public class PostMember extends ReplicaMember {
+
+    public PostMember(String username, String password, String nickname) {
+        super(username, password, nickname);
+    }
+
 }
