7aea9775f36fcbaa6839bdaee110aa204198150b
YooSejeong <nived3@naver.com>
2025-12-24 17:38:03 +0900

    0019

--- src/main/java/com/back/boundedContext/post/domain/PostMember.java
+++ src/main/java/com/back/boundedContext/post/domain/PostMember.java
@@ -5,13 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @NoArgsConstructor
 @Table(name = "POST_MEMBER")
 public class PostMember extends ReplicaMember {
 
-    public PostMember(String username, String password, String nickname) {
-        super(username, password, nickname);
+    public PostMember(int id, LocalDateTime createDate, LocalDateTime modifyDate, String username, String password, String nickname) {
+        super(id, createDate, modifyDate, username, password, nickname);
     }
 
 }
