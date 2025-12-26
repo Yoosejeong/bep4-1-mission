b09b1944824a22e1299bf0cd8b7b600bdd3c13e1
YooSejeong <nived3@naver.com>
2025-12-26 14:29:23 +0900

    0025

--- src/main/java/com/back/boundedContext/cash/out/WalletRepository.java
+++ src/main/java/com/back/boundedContext/cash/out/WalletRepository.java
@@ -1,9 +1,13 @@
 package com.back.boundedContext.cash.out;
 
+import com.back.boundedContext.cash.domain.CashMember;
 import com.back.boundedContext.cash.domain.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Integer> {
+    Optional<Wallet> findByHolder(CashMember holder);
 }
