578563c06a6f77befe88a2f4b01a9b9e71d99cce
YooSejeong <nived3@naver.com>
2025-12-29 16:21:35 +0900

    0036

--- src/main/java/com/back/boundedContext/market/domain/Order.java
+++ src/main/java/com/back/boundedContext/market/domain/Order.java
@@ -27,6 +27,7 @@ public class Order extends BaseIdAndTime {
     private MarketMember buyer;
     private LocalDateTime requestPaymentDate;
     private LocalDateTime paymentDate;
+    private LocalDateTime cancelDate;
     private long price;
     private long salePrice;
 
@@ -76,5 +77,13 @@ public class Order extends BaseIdAndTime {
 
     public boolean isPaid() {
         return paymentDate != null;
+    }
+
+    public boolean isCanceled() {
+        return cancelDate != null;
+    }
+
+    public boolean isPaymentInProgress() {
+        return requestPaymentDate != null && paymentDate == null && cancelDate == null;
     }
 }
